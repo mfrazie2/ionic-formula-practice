f2f86e9cc755b2a68775fb1ba5259a949fc853b5
mfrazie2 <michael.s.frazier@gmail.com>
2023-05-25 16:40:50 -0600

    Create common footer
    * Add react-icons
    * Create mike does coding icon
      * TODO: Improve this icon

---
+++ components/common/footer/index.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { MikeDoesCoding } from "@/components/icons/mikedoescoding";
+import { Icon } from "@chakra-ui/icon";
+Image
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import Image from "next/image";
+import Link from "next/link";
+import { FaEnvelope, FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+
+export default function Footer() {
+    return (
+        <Box w="100%" bg="slateGray">
+            <Flex as="footer" dir='row' alignItems="center" justifyContent="space-between" maxW="1280px" px="80px" h="80px" mx="auto">
+                <Text color="spaceCadet">Coded from a couch (probably) | &copy; 2023</Text>
+                <Flex dir="row" columnGap="20px">
+                    <Link href="mailto:me@mikedoescoding.com" aria-label="Send an email to creator">
+                        <Icon as={FaEnvelope} boxSize={5} color="spaceCadet" />
+                    </Link>
+                    <Link href="https://github.com/mfrazie2" target="__blank" aria-label="View creator's github account">
+                        <Icon as={FaGithubSquare} boxSize={5} color="spaceCadet" />
+                    </Link>
+                    <Link href="https://linkedin.com/in/mikesfrazier" target="__blank" aria-label="View creator's linkedin account">
+                        <Icon as={FaLinkedin} boxSize={5} color="spaceCadet" />
+                    </Link>
+                    <Link href="https://mikedoescoding.com/about" target="__blank" aria-label="View creator's about me page">
+                        <MikeDoesCoding boxSize={5} color="spaceCadet" />
+                    </Link>
+                </Flex>
+            </Flex>
+        </Box>
+    );
+}
