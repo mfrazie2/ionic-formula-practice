84b0d1854011c4de9911aa305e4e205a96f7806f
mfrazie2 <michael.s.frazier@gmail.com>
2023-07-03 12:05:10 -0600

    Use a mediaquery to determine mobile size
    * Add to footer and learn page

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import { Tab, TabList, TabPanel, TabPanels, Tabs, useMediaQuery } from "@chakra-ui/react";
 import MonatomicIonExamples from "./monatomic-ion-examples";
 import PolyatomicIonExamples from "./polyatomic-ion-examples";
 import TransitionMetalIonExamples from "./transition-metal-ion-examples";
 import FormulaToNameExamples from "./formula-to-name";
 import ContentWrapper from "@/components/common/content-wrapper";
 
-/**
- * Maybe use useMediaQuery to either show the TabList (large viewport) or Menu (smaller viewports)
- * Not sure how the menu would influence the displayed tab 
- * https://chakra-ui.com/docs/components/tabs#controlled-tabs
- */
-
 export default function Learn() {
+  const [isMobile] = useMediaQuery('(max-width: 800px)', {
+    ssr: false,
+    fallback: false, // return false on the server, and re-evaluate on the client side
+  });
+
   return (
     <ContentWrapper>
-      <Tabs isLazy>
+      <Tabs isLazy isFitted size={isMobile ? "sm" : 'md'}>
         <TabList>
           <Tab>Monatomic Ions</Tab>
           <Tab>Polyatomic Ions</Tab>
--- components/common/footer/index.tsx
+++ components/common/footer/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Icon } from "@chakra-ui/icon";
+import { useMediaQuery } from '@chakra-ui/react'
 Image
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import Image from "next/image";
@@ -8,11 +9,16 @@ import Link from "next/link";
 import { FaEnvelope, FaGithubSquare, FaLinkedin, FaHome } from 'react-icons/fa'
 
 export default function Footer() {
+    const [isMobile] = useMediaQuery('(max-width: 800px)', {
+        ssr: false,
+        fallback: false, // return false on the server, and re-evaluate on the client side
+    });
+
     return (
         <Box w="100%" bg="slateGray">
-            <Flex as="footer" dir='row' alignItems="center" justifyContent="space-between" maxW={["100%", "1280px"]} px={[2, 4, 6, 8]} h="80px" mx="auto">
+            <Flex as="footer" direction={isMobile ? 'column' : 'row'} alignItems="center" justifyContent={isMobile ? "space-around" : "space-between"} maxW={["100%", "1280px"]} px={[2, 4, 6, 8]} h="80px" mx="auto">
                 <Text color="spaceCadet">Coded from a couch (probably) | &copy; 2023</Text>
-                <Flex dir="row" columnGap="20px">
+                <Flex direction="row" columnGap="20px">
                     <Link href="mailto:me@mikedoescoding.com" aria-label="Send an email to creator">
                         <Icon as={FaEnvelope} boxSize={5} color="spaceCadet" />
                     </Link>
